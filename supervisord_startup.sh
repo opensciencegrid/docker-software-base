18997c58f120704c2f724bdf3a32d4ff4a5cb9ee
Brian Lin <brianhlin@gmail.com>
2019-12-18 15:02:07 -0600

    Log image init script stdout/stderr

--- supervisord_startup.sh
+++ supervisord_startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Allow the derived images to run any additional runtime customizations
-for x in /etc/osg/image-config.d/*.sh; do source "$x"; done
+for x in /etc/osg/image-config.d/*.sh; do source "$x"; done > /var/log/supervisord_startup.out 2> /var/log/supervisord_startup.err
 
 # Now we can actually start the supervisor
 exec /usr/bin/supervisord -c /etc/supervisord.conf
