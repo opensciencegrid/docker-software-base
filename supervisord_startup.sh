dd8c15590e7bc02b5445bb46746e0527c3cfa5c9
edquist <edquist@users.noreply.github.com>
2021-08-18 18:40:52 -0500

    use nullglob for image-init.d globbing (SOFTWARE-4749)
    lamely typed into the github web ui!

--- supervisord_startup.sh
+++ supervisord_startup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Allow the derived images to run any additional runtime customizations
+shopt -s nullglob
 for x in /etc/osg/image-init.d/*.sh; do source "$x"; done
+shopt -u nullglob
 
 # Allow child images to add cleanup customizations
 function source_cleanup {
