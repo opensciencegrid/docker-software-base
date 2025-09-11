00b6cc04b88c464969c388a0107883663370b350
Matyas Selmeci <mselmeci@wisc.edu>
2025-09-10 20:25:04 -0500

    Exit container_cleanup.sh early if there's nothing to do; also don't autorestart it

--- container_cleanup.sh
+++ container_cleanup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [[ -z $(shopt -s nullglob && echo /etc/osg/image-cleanup.d/*.sh) ]]; then
+    exit 0
+fi
+
 # Allow child images to add cleanup customizations
 source_cleanup () {
     for x in /etc/osg/image-cleanup.d/*.sh; do source "$x"; done
